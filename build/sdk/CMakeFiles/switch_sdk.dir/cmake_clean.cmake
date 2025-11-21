ce177418b9ae95490e178505a2b5ba6cc4a188e2
Antithetical <arthur.guelennoc@epita.fr>
2025-11-21 16:55:29 +0100

    feat: setup cmake root and sdk

---
+++ build/sdk/CMakeFiles/switch_sdk.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/switch_sdk.dir/AgentBase.cpp.o"
+  "CMakeFiles/switch_sdk.dir/AgentBase.cpp.o.d"
+  "CMakeFiles/switch_sdk.dir/StateDB.cpp.o"
+  "CMakeFiles/switch_sdk.dir/StateDB.cpp.o.d"
+  "libswitch_sdk.a"
+  "libswitch_sdk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/switch_sdk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
